b7a440342c409637d66cf003b2fedfc8e36d364e
SPOOPINATURAL <98136495+SPOOPINATURAL@users.noreply.github.com>
2025-06-17 11:10:37 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,9 +1,6 @@
 #!/bin/zsh
 cd /home/spoopinatural/jagerbot
 git pull origin main
-git rm --cached -r __pycache__/
-git rm --cached -r cogs/__pycache__/
-git rm --cached -r utils/__pycache__/
 find . -type d -name "__pycache__" -exec rm -rf {} +
 source ~/jagerbot/venv/bin/activate
 pip install -U -r requirements.txt
