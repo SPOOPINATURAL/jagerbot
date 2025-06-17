26f7f6410abd087a68a7ab71603d83a5f142fa6d
SPOOPINATURAL <98136495+SPOOPINATURAL@users.noreply.github.com>
2025-06-17 10:52:04 -0500

    better update script

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
+set -e
 cd /home/spoopinatural/jagerbot
 git pull origin main
 find . -type d -name "__pycache__" -exec rm -rf {} +
+source ~/jagerbot/venv/bin/activate
+pip install -U -r requirements.txt
 sudo systemctl restart jagerbot
