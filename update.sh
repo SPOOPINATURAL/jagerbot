351854ca6af50e05334bdda05be6f78c4986c3c1
SPOOPINATURAL <98136495+SPOOPINATURAL@users.noreply.github.com>
2025-06-17 10:57:12 -0500

    get rid of cached stuff

--- update.sh
+++ update.sh
@@ -2,6 +2,9 @@
 set -e
 cd /home/spoopinatural/jagerbot
 git pull origin main
+git rm --cached -r __pycache__/
+git rm --cached -r cogs/__pycache__/
+git rm --cached -r utils/__pycache__/
 find . -type d -name "__pycache__" -exec rm -rf {} +
 source ~/jagerbot/venv/bin/activate
 pip install -U -r requirements.txt
