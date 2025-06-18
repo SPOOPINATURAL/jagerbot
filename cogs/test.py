076be6155552fc9621ed077f6613bb54bf339913
SPOOPINATURAL <98136495+SPOOPINATURAL@users.noreply.github.com>
2025-06-18 17:53:47 -0500

    test

--- cogs/test.py
+++ cogs/test.py
@@ -2,6 +2,11 @@ from discord.ext import commands
 import discord
 
 class TestCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.bot.tree.add_command(
+            discord.SlashCommandGroup(name="testgroup", description="Test group")
+        )
     @commands.slash_command(name='hello', description="Hello!")
     async def hello(self, ctx: discord.ApplicationContext):
         await ctx.respond(f"Hallo {ctx.user.mention} :)")
