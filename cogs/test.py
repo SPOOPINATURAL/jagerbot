7b3c97c987b65a4c79b5d6349c11fcec7b0c2ceb
SPOOPINATURAL <98136495+SPOOPINATURAL@users.noreply.github.com>
2025-06-18 20:00:48 -0500

    Update test.py

--- cogs/test.py
+++ cogs/test.py
@@ -8,13 +8,17 @@ class TestCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(name="test", description="Test command group", invoke_without_command=True)
-    async def test(self, ctx: commands.Context):
-        await ctx.respond("Use a subcommand, like `/test hello`")
+    @discord.slash_command(name="test", description="Test command group")
+    async def test(self, ctx: discord.ApplicationContext):
+        await ctx.respond("Use a subcommand like `/test hello`")
 
-    @test.command(name="hello", description="Say hello!")
-    async def hello(self, ctx: commands.Context):
-        await ctx.respond(f"Hallo {ctx.author.mention} :)")
+    @test.sub_command(name="hello", description="Say hello!")
+    async def hello(self, ctx: discord.ApplicationContext):
+        await ctx.respond(f"Hallo {ctx.user.mention} :)")
+
+    @test.sub_command(name="bye", description="Say goodbye!")
+    async def bye(self, ctx: discord.ApplicationContext):
+        await ctx.respond(f"Tschüss {ctx.user.mention} 👋")
 
 def setup(bot):
     bot.add_cog(TestCog(bot))
