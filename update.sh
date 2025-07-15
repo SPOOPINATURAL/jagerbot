61b2a4b227b3dde82de50d801353cb9033289ac7
SPOOPINATURAL <98136495+SPOOPINATURAL@users.noreply.github.com>
2025-07-15 01:45:08 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
+set -euo pipefail
 cd /home/spoopinatural/jagerbot
 git pull origin main
 find . -type d -name "__pycache__" -exec rm -rf {} +
-~/jagerbot/venv/bin/pip install -U -r requirements.txt
+/home/spoopinatural/jagerbot/venv/bin/pip install -U -r requirements.txt
 sudo systemctl restart jagerbot.service
